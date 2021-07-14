e8a0552037b0f81c31399534fefc71cfc974a338
AbhikS847 <abhikshrestha@gmail.com>
2021-07-14 16:36:32 +1000

    Had confusion setting state

--- frontend/src/components/CreateUsers.js
+++ frontend/src/components/CreateUsers.js
@@ -17,6 +17,10 @@ const CreateUsers = () => {
         ]
     })
 
+    const handleClick = () =>{
+        console.log(state.Student);
+    }
+
    useEffect(()=>{
         const getOptions = async() =>{
             const res = await axios.get('http://localhost:5000/teachers');
@@ -44,18 +48,24 @@ const CreateUsers = () => {
                 <Row>
                     <Col className="my-2" xs={12} sm={4}>
                     <Form.Label><b>Fullname</b></Form.Label>
-                    <Form.Control placeholder="Example - John Smith" value={state.Student.Fullname}/>
+                    <Form.Control placeholder="Example - John Smith" defaultValue={state.Student.Fullname} onChange={(event)=>{
+                        state.Student.Fullname = event.target.value;
+                    }}/>
                     </Col>
                     <Col className="my-2" xs={12} sm={8}>
                     <Form.Label><b>Interests</b></Form.Label>
-                    <Form.Control placeholder="Type in your interested areas" value={state.Student.Interests}/>
+                    <Form.Control placeholder="Type in your interested areas" defaultValue={state.Student.Interests} onChange={(event)=>{
+                        state.Student.Interests = event.target.value;
+                    }}/>
                     <Form.Text className="d-flex justify-content-center" muted>
                     Seperate each interests with a comma. For example - Business, Arts, Science,Engineering, Health...
   </Form.Text>
                     </Col>
                     <Col xs={12} sm={4} className="my-2">
                     <Form.Label><b>Teacher(Select)</b></Form.Label>
-                    <Form.Control as="select">
+                    <Form.Control as="select" onChange={(event)=>{
+                        state.Student.Teacher = event.target.value
+                    }} >
                     {state.Teacher.map((teacher)=>{
                         return(
                             <option key={teacher._id}>{teacher.Fullname}</option>
@@ -68,20 +78,20 @@ const CreateUsers = () => {
                     </Col>
                     <hr></hr>
                     <div className="d-flex justify-content-center">
-                    <Form.Check className=" px-3"
+                    <Form.Check onClick={()=>{state.Student.Enrolled=true}}className=" px-3"
           type="radio"
           label="Enrolled"
           name="formHorizontalRadios"
           id="formHorizontalRadios1"
         />
-        <Form.Check className="px-3"
+        <Form.Check onClick={()=>{state.Student.Enrolled=false}} className="px-3"
           type="radio"
           label="Unenrolled"
           name="formHorizontalRadios"
           id="formHorizontalRadios2"
         />
                     </div>
-                    <Button type="" className="my-2" style={{width:'50%',marginLeft:'25%'}}>Create Student</Button>
+                    <Button onClick={handleClick} type="" className="my-2" style={{width:'50%',marginLeft:'25%'}}>Create Student</Button>
                 </Row>
             </Form>
             </Container>
