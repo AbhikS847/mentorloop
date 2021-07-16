f1531589c8def490473a2568734a2b1ffba4dcee
AbhikS847 <abhikshrestha@gmail.com>
2021-07-16 13:46:06 +1000

    Update teacher route completed

--- frontend/src/components/updatefilters/UpdateTeacher.js
+++ frontend/src/components/updatefilters/UpdateTeacher.js
@@ -32,6 +32,16 @@ const UpdateTeacher = () => {
         getTeachers();
     },[])
 
+    const handleClick = async()=>{
+        console.log(state.teacher);
+        const res = await axios.put('http://localhost:5000/update/teacher',{
+            id:state.teacher._id,
+            Fullname:state.teacher.Fullname,
+            Experience:state.teacher.Experience,
+            Weekends:state.teacher.Weekends,
+        });
+    }
+
     return (
         <div>
  <Form>
@@ -48,9 +58,43 @@ const UpdateTeacher = () => {
                     })}
       </Form.Control>
       <Form.Text className="d-flex justify-content-center" muted>
-      Select a student from the dropdown list
+      Select a Teacher from the dropdown list
   </Form.Text>
                     </Col>
+                    <Col className="my-2" xs={12} sm={4}>
+                    <Form.Label><b>Edit Fullname</b></Form.Label>
+                    <Form.Control placeholder="Example - John Smith" defaultValue={state.teacher.Fullname} onChange={(event)=>{
+                        state.teacher.Fullname = event.target.value;
+                    }}/>
+                          <Form.Text className="d-flex justify-content-center" muted>
+                          Enter a name to update existing Teacher
+  </Form.Text>
+                    </Col>
+                    <Col className="my-2" xs={12} sm={4}>
+                    <Form.Label><b>Edit Experience (In years)</b></Form.Label>
+                    <Form.Control type="number" placeholder="Example - John Smith" defaultValue={state.teacher.Experience} onChange={(event)=>{
+                        state.teacher.Experience = event.target.value;
+                    }}/>
+                          <Form.Text className="d-flex justify-content-center" muted>
+                          Enter a name to update existing Teacher
+  </Form.Text>
+                    </Col>
+                    <hr></hr>
+                    <div className="d-flex justify-content-center">
+                    <Form.Check onClick={()=>{state.teacher.Weekends=true}}className=" px-3"
+          type="radio"
+          label="Available on weekends"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios1"
+        />
+        <Form.Check onClick={()=>{state.teacher.Weekends=false}} className="px-3"
+          type="radio"
+          label="Unavailable on weekends"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios2"
+        />
+                    </div>
+                    <Button onClick={handleClick} type="" className="my-2" style={{width:'50%',marginLeft:'25%'}}>Update Teacher</Button>
                     </Row>
                     </Form>
         </div>
